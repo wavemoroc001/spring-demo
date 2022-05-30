2ce5502552ce20f152a49e61cb76963001d19c9e
wavemoroc001 <patipan.m@appman.co.th>
2022-05-30 23:22:42 +0700

    initial project

---
+++ src/main/java/com/example/springdemo/TestController.java
@@ -0,0 +1,14 @@
+package com.example.springdemo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello");
+    }
+}
